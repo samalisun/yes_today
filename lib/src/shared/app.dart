62e0bb17872c81c9762d8e02284bc0dec2b3d5f0
Sam.A Sun <sameenalisun@gmail.com>
2022-12-16 00:22:59 +0800

    [Feature] Theme - Implement theme data and change default to dark

--- lib/src/shared/app.dart
+++ lib/src/shared/app.dart
@@ -1,27 +1,52 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../features/today/view/today_screen.dart';
 import 'providers/providers.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final settings = ValueNotifier(ThemeSettings(
+    sourceColor: Colors.lightBlue,
+    themeMode: ThemeMode.dark,
+  ));
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: ((context) => TodosProvider()),
-      child: MaterialApp(
-        title: 'Yes!Today',
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-          ),
-          useMaterial3: true,
-        ),
-        home: const TodayScreen(),
-      ),
+      create: ((context) => TasksProvider()),
+      child: DynamicColorBuilder(
+          builder: (lightDynamic, darkDynamic) => ThemeProvider(
+              settings: settings,
+              lightDynamic: lightDynamic,
+              darkDynamic: darkDynamic,
+              child: NotificationListener<ThemeSettingChange>(
+                onNotification: (notification) {
+                  settings.value = notification.settings;
+                  return true;
+                },
+                child: ValueListenableBuilder<ThemeSettings>(
+                  valueListenable: settings,
+                  builder: (context, value, _) {
+                    final theme = ThemeProvider.of(context);
+
+                    return MaterialApp(
+                      title: 'Yes!Today',
+                      theme: theme.light(settings.value.sourceColor),
+                      darkTheme: theme.dark(settings.value.sourceColor),
+                      themeMode: theme.themeMode(),
+                      home: const TodayScreen(),
+                    );
+                  },
+                ),
+              ))),
     );
   }
 }
