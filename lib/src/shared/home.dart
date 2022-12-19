5bc1fb4c4bf68b4c15db8db592d36fc1972bef14
Sam.A Sun <sameenalisun@gmail.com>
2022-12-19 23:49:16 +0800

    [Feature] add drawer

--- lib/src/shared/home.dart
+++ lib/src/shared/home.dart
@@ -15,12 +15,14 @@ class TodayScreen extends StatefulWidget {
 class _TodayScreenState extends State<TodayScreen> {
   final TextEditingController _textFieldController = TextEditingController();
   final List<String> titleList = ['Home', 'Today', 'Review'];
+  final List<Icon> iconList = [
+    const Icon(Icons.filter_alt_outlined),
+    const Icon(Icons.sticky_note_2_outlined),
+    const Icon(Icons.lightbulb_outline),
+  ];
 
   String newTaskTitle = '';
   bool newTaskCompleted = false;
-
-  double? scrolledUnderElevation;
-  bool shadowColor = false;
 
   int currentPageIndex = 0;
   String title = 'Home';
@@ -76,22 +78,98 @@ class _TodayScreenState extends State<TodayScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        scrolledUnderElevation: scrolledUnderElevation,
-        shadowColor: shadowColor ? Theme.of(context).colorScheme.shadow : null,
         title: Text(title),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
-        ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.account_circle_outlined),
+            icon: iconList[currentPageIndex],
             onPressed: () {},
           ),
         ],
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color.fromRGBO(33, 36, 42, 1),
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              child: DrawerHeader(
+                padding: const EdgeInsets.all(0),
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(33, 36, 42, 1),
+                  border: null,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text('YesToday', style: TextStyle(fontSize: 18)),
+                    Container(
+                      margin: const EdgeInsets.only(top: 24),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      decoration: BoxDecoration(
+                        color: colors.surface,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: colors.primaryContainer,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const <Widget>[
+                                  Text('Kristen', style: TextStyle(fontSize: 16)),
+                                  Padding(padding: EdgeInsets.only(bottom: 4)),
+                                  Text('Developer', style: TextStyle(fontSize: 12)),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Icon(Icons.settings)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const ListTile(
+              title: Text('Projects'),
+            ),
+            ListTile(
+              // contentPadding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+              leading: const Icon(Icons.folder),
+              title: const Text('Webdex'),
+              selected: true,
+              selectedTileColor: colors.primaryContainer,
+              selectedColor: colors.onPrimaryContainer,
+              onTap: () {
+                Navigator.pop(context);
+              },
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(100))),
+            ),
+            ListTile(
+              // contentPadding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+              leading: const Icon(Icons.folder_outlined),
+              title: const Text('QAclub'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: <Widget>[
         const HomeTasks(),
